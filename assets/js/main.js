7e1cc594c6cbe56ef06dc0f57652fa2a069aaf53
holdfastfingers <steven.pingree5@gmail.com>
2025-03-09 16:06:37 -0500

    text fetch back to own js

--- assets/js/main.js
+++ assets/js/main.js
@@ -327,13 +327,7 @@ document.addEventListener("DOMContentLoaded", function() {
     nextBtn.addEventListener('click', function() {
         gallery.scrollBy({ left: 650, behavior: 'smooth' });
     });
-
-    fetch('text/John/john_card_body.txt')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('john-card-body').innerText = data;
-        })
-        .catch(error => console.error('Error fetching the text file:', error));
+  
 });
 
 let scrollContainer=document.querySelector('.gallery');
