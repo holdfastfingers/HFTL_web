2dfceeb30d6d91941cbf85327ee894f00c655ac9
holdfastfingers <steven.pingree5@gmail.com>
2025-03-09 16:09:22 -0500

    text fetch in main

--- assets/js/main.js
+++ assets/js/main.js
@@ -327,6 +327,13 @@ document.addEventListener("DOMContentLoaded", function() {
     nextBtn.addEventListener('click', function() {
         gallery.scrollBy({ left: 650, behavior: 'smooth' });
     });
+
+	fetch('text/john_card_body.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('john-card-body').innerText = data;
+        })
+        .catch(error => console.error('Error fetching the text file:', error));
   
 });
 
--- assets/js/textfetch.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('text/john_card_body.txt')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('john-card-body').innerText = data;
-        })
-        .catch(error => console.error('Error fetching the text file:', error));
-});
